e83141f0c117f255143e3794d6ae53e2c0a8158b
RicardoMoya <RicardoMoya>
2015-12-20 18:13:05 +0100

    primer commit

---
+++ src/Main/Main.java
@@ -0,0 +1,28 @@
+/**
+ * @author RicardoMoya
+ */
+
+package Main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        SoyUnico ricardo = SoyUnico.getSingletonInstance("Ricardo Moya");
+        SoyUnico ramon = SoyUnico.getSingletonInstance("Ramón Invarato");
+        
+        // ricardo y ramon son referencias a un único objeto de la clase SoyUnico
+        System.out.println(ramon.getNombre());
+        System.out.println(ricardo.getNombre());
+        
+        // No se permite clonar un objeto de esta clase
+        try{
+            SoyUnico richard = ricardo.clone();
+            System.out.println(richard.getNombre());
+        }catch (NullPointerException ex){
+            ex.printStackTrace();
+        }
+
+    }
+
+}
